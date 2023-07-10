c3763da77f4c490abb261c9100e3ef47c3135ed0
Don-Smilo <don_smilo@mail.ru>
2023-07-10 19:19:58 +0300

    created crosshair
    also increased projectile speed and shooting vertical angle

--- Source/Practice/PracticeCharacter.cpp
+++ Source/Practice/PracticeCharacter.cpp
@@ -99,7 +99,7 @@ void APracticeCharacter::Fire()
 
 		// Skew the aim to be slightly upwards
 		FRotator MuzzleRotation = CameraRotation;
-		MuzzleRotation.Pitch += 10.0f;
+		MuzzleRotation.Pitch += 2.0f;
 
 		UWorld* World = GetWorld();
 		if (World)
